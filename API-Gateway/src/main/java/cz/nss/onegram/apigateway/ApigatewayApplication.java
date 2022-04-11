3aefaed0dfade929978292fbe79398edf71cf5b4
premekb <38076249+premekb@users.noreply.github.com>
2022-04-11 15:07:27 +0200

    Created Gateway service

---
+++ API-Gateway/src/main/java/cz/nss/onegram/apigateway/ApigatewayApplication.java
@@ -0,0 +1,11 @@
+package cz.nss.onegram.apigateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApigatewayApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ApigatewayApplication.class, args);
+    }
+}
---
+++ API-Gateway/src/main/java/cz/nss/onegram/apigateway/config/RouteConfiguration.java
@@ -0,0 +1,7 @@
+package cz.nss.onegram.apigateway.config;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RouteConfiguration {
+}
