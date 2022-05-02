42c491e6ece07625e7dc5727f490e09d8a990a6d
Phuong Dong Cu <cuphuon3@fel.cvut.cz>
2022-05-02 21:40:14 +0000

    18 post security

--- User/src/main/java/cz/nss/onegram/user/config/WebSecurityConfig.java
+++ User/src/main/java/cz/nss/onegram/user/config/WebSecurityConfig.java
@@ -1,12 +1,16 @@
 package cz.nss.onegram.user.config;
 
 import cz.nss.onegram.user.security.OAuth2SuccessHandler;
+import cz.nss.onegram.user.security.jwt.AuthTokenFilter;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 
 @Configuration
@@ -17,12 +21,21 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Autowired
     private OAuth2SuccessHandler successHandler;
 
+    @Bean
+    public AuthTokenFilter authenticationJwtTokenFilter() {
+        return new AuthTokenFilter();
+    }
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.authorizeRequests()
+        http
+                .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
+                .authorizeRequests()
                 .anyRequest().authenticated()
                 .and()
-                .oauth2Login()
-                .successHandler(successHandler);
+                .oauth2Login().successHandler(successHandler);
+
+        http.addFilterBefore(authenticationJwtTokenFilter(), UsernamePasswordAuthenticationFilter.class);
     }
 }
