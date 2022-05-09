155bb16f2d7e977e1ee726cc8ce3ed2fcef82d38
bureson2@fel.cvut.cz <bureson2@fel.cvut.cz>
2022-05-09 23:57:13 +0200

    Bez erroru v consoli, ale presto nefunkcni

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,7 +6,6 @@ import {Profile, Profile_dashboard} from "./pages";
 
 // import {UserRoutes} from "./routes/UserRoutes.js";
 
-
 export const App = (props) => {
     function useRoutes(props) {
         return undefined;
