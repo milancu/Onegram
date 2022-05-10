eba63b39f35134f8fd1c72317b13137e861a3d3f
Jan Pivoňka <pivonja1@fel.cvut.cz>
2022-05-10 21:50:15 +0200

    displaying comments, responses and comment form

--- frontend/src/components/Comments/CommentForm.js
+++ frontend/src/components/Comments/CommentForm.js
@@ -1,8 +1,23 @@
-const CommentForm = () => {
+import {useState} from "react";
+import "./CommentForm.css"
+
+const CommentForm = ({handleSubmit, submitLabel}) => {
+    const [text, setText] = useState("");
+    const isTextAreaDisabled = text.length === 0;
+    const onSubmit = event => {
+        event.preventDefault()
+        handleSubmit(text)
+        setText("")
+    }
     return (
-        <div>
-            CommentForm
-        </div>
+        <form className="comment-form-wrapper" onSubmit={onSubmit}>
+            <input type="text" className="comment-form-textarea"
+                      placeholder="Add comment..."
+                      value={text}
+                      onChange={(e) => setText(e.target.value)}
+            />
+            <button className="comment-form-button" disabled={isTextAreaDisabled}>{submitLabel}</button>
+        </form>
     );
 };
 
--- frontend/src/components/Comments/Comments.js
+++ frontend/src/components/Comments/Comments.js
@@ -1,9 +1,21 @@
 import {useEffect, useState} from "react";
 import Comment from "./Comment";
+import CommentForm from "./CommentForm";
 
 const Comments = (props) => {
     const [postComments, setPostComments] = useState([]);
     const rootComments = postComments.filter((postComment) => postComment.parentId === null);
+    const getReplies = commentId => {
+        return postComments.filter((postComment) => postComment.parentId === commentId);
+    }
+    const addComment = (text, parentId) => {
+        console.log('addComment', text, parentId)
+    }
+    const createComment = async (text, parentId = null) => {
+        return {
+            "id": Math.random()
+        }
+    }
 
     useEffect(() => {
         setPostComments(props.comments);
@@ -11,9 +23,12 @@ const Comments = (props) => {
     return (
         <div className="comments">
             <hr />
+            <CommentForm submitLabel="Write" handleSubmit={addComment}/>
             <div className="comments_container">
                 {rootComments.map(rootComment => (
-                    <Comment key={rootComment.id} comment={rootComment}/>
+                    <Comment key={rootComment.id}
+                             comment={rootComment}
+                             replies={getReplies(rootComment.id)}/>
                 ))}
             </div>
         </div>
