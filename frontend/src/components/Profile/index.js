a8b5422c46d10b5f10332edb5c62e1890d988fa1
bureson2@fel.cvut.cz <bureson2@fel.cvut.cz>
2022-05-10 20:43:39 +0200

    profile dashboard

--- frontend/src/components/Profile/index.js
+++ frontend/src/components/Profile/index.js
@@ -2,6 +2,7 @@
 
 import React from "react";
 import "./profile.css";
+import {Link} from "react-router-dom";
 
 
 export const Profile = (props) => {
@@ -9,17 +10,51 @@ export const Profile = (props) => {
     const nickname = props.nickname;
     const profilepicture = props.profilepicture;
     const description = props.description;
+    const webLinkUrl = props.webLinkUrl;
+    const follows = props.follows;
+    const followers = props.followers
+    const postsNumber = props.postsNumber;
+
 
     return (
         <section className="profile-description">
-            <div className="Post-user-profilepicture">
-                <img src={profilepicture} alt={nickname} className="Post-user-profilepicture"/>
+            <div className="profile-photo">
+                <div className={"profile-main"}>
+                    <div className={"profile-user-profilepicture"}>
+                        <img src={profilepicture} alt={nickname} className="profile-user-profilepicture"/>
+                    </div>
+                    <h3>{nickname}</h3>
+                </div>
+
 
             </div>
-            {/*    TODO */}
+            <div className="profile-info">
+                <div className="Profile-bio"><p>{description}</p></div>
+                <div className="Profile-web">
+                    {/*TODO nefunguje*/}
+                    <a href={'www.seznam.cz'}>
+                        <p>{webLinkUrl}</p>
+                    </a>
+                </div>
+
+            </div>
+            <div className="profile-stats">
+                <div className={"statsContainer"}>
+                    <p>Posts</p>
+                    <p>{postsNumber}</p>
+                </div>
+
+                <div className={"statsContainer"}>
+                    <p>Followers</p>
+                    <p>{followers}</p>
+                </div>
+                <div className={"statsContainer"}>
+                    <p>Follows</p>
+                    <p>{follows}</p>
+                </div>
+            </div>
         </section>
     )
-
 }
 
 export default Profile
