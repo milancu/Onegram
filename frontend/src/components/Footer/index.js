82eade7afe16d5618ca364b47c2f441bd07be94d
bureson2@fel.cvut.cz <bureson2@fel.cvut.cz>
2022-05-11 16:49:18 +0200

    kompletni proroutovani, profile Dashborad jeste bez obrazku

---
+++ frontend/src/components/Footer/index.js
@@ -0,0 +1,18 @@
+// src/components/Footer/index.js
+
+import React from "react";
+import "./Footer.css";
+import {Link, useNavigate} from "react-router-dom";
+import logo from "../../images/logo.png";
+
+export const Footer = (props) => {
+    return (
+        <nav>
+        {/*    Todo*/}
+        </nav>
+    );
+}
+
+
+
+export default Footer;
