61e4eef9464f730e77cc9ba541d48acdb39eb6cf
Phuong Dong Cu <cuphuon3@fel.cvut.cz>
2022-05-25 22:14:04 +0200

    add handling and preauthorize

--- User/src/main/java/cz/nss/onegram/user/service/interfaces/UserService.java
+++ User/src/main/java/cz/nss/onegram/user/service/interfaces/UserService.java
@@ -9,8 +9,6 @@ import java.util.List;
 
 public interface UserService {
 
-    public List<User> getAllUsers();
-
     public User findById(int id);
 
     public User findByEmail(String email);
@@ -21,7 +19,7 @@ public interface UserService {
 
     public User getCurrentUser();
 
-    public User followUser(int userToFollowId);
+    public void followUser(int userToFollowId);
 
     public void unFollowUser(int userToUnFollowId);
 
@@ -47,5 +45,13 @@ public interface UserService {
 
     public void editBio(String bio);
 
-    public void addPhoto(User user, InputStream file);
+    public void addPhoto(InputStream file);
+
+    public boolean isFollowingUser(int userID);
+
+    public boolean isFollowedByUser(int userID);
+
+    public boolean hasSentRequest(int requestID);
+
+    public boolean hasReceivedRequest(int requestID);
 }
