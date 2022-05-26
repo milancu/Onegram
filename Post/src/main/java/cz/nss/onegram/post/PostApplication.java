748aa77fdb8f1023fc1cc9d10d20682ac593bdb9
Přemek Bělka <belkapre@fel.cvut.cz>
2022-05-26 12:05:53 +0000

    20 cache

--- Post/src/main/java/cz/nss/onegram/post/PostApplication.java
+++ Post/src/main/java/cz/nss/onegram/post/PostApplication.java
@@ -2,8 +2,12 @@ package cz.nss.onegram.post;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableCaching
+@EnableScheduling
 public class PostApplication {
     public static void main(String[] args) {
         SpringApplication.run(PostApplication.class, args);
