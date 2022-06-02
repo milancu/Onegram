34b42c1b6e46a97530d5c06eff128cc9a60b27ca
Jan Pivoňka <pivonja1@fel.cvut.cz>
2022-06-02 21:14:11 +0200

    slight change

--- frontend/src/components/Post/index.js
+++ frontend/src/components/Post/index.js
@@ -51,7 +51,7 @@ export const Post = (props) => {
                         "likeAmount": 5
                     },
                     {
-                        "id": "2",
+                        "id": 2,
                         "parentId": 1,
                         "content": "hi",
                         "userImage": "https://t4.ftcdn.net/jpg/02/19/63/31/360_F_219633151_BW6TD8D1EA9OqZu4JgdmeJGg4JBaiAHj.jpg",
