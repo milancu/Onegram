13164e0c5f1effb6eeb20f8ff26aa950b0589b44
bureson2@fel.cvut.cz <bureson2@fel.cvut.cz>
2022-06-06 19:07:22 +0200

    zprovozneni follow requestu
    #todo now pridat buttony do profilu

--- frontend/src/components/Single_follow/index.js
+++ frontend/src/components/Single_follow/index.js
@@ -1,10 +1,52 @@
 import React from "react";
 import {Link} from "react-router-dom";
+import axios from "axios";
+import * as Constants from "../../gql/query";
 
 const testname = "Buri"
 const testImage = "https://t4.ftcdn.net/jpg/02/19/63/31/360_F_219633151_BW6TD8D1EA9OqZu4JgdmeJGg4JBaiAHj.jpg"
 
 export const Single_follow = ({follow}) => {
+
+    function unfollowUser() {
+        const UNFOLLOW_USER = `
+        mutation UNFOLLOW_USER{
+            unFollowUser(input:{
+                userId:` + follow.id + `
+            })
+        }
+        `;
+        axios.post(Constants.USER_GRAPHQL_API, {
+                query: UNFOLLOW_USER
+            },
+            {
+                headers: {
+                    "Authorization": "Bearer " + localStorage.getItem('token')
+                }
+            })
+            .then(res => console.log(res))
+            .catch(err => console.log(err))
+    }
+
+    function followUser() {
+        const FOLLOW_USER = `
+        mutation FOLLOW_USER{
+            followUser(input:{
+                userId:` + follow.id + `
+            })
+        }
+        `;
+        axios.post(Constants.USER_GRAPHQL_API, {
+                query: FOLLOW_USER
+            },
+            {
+                headers: {
+                    "Authorization": "Bearer " + localStorage.getItem('token')
+                }
+            })
+            .then(res => console.log(res))
+            .catch(err => console.log(err))
+    }
 
     return (
         <div className={"follower-box"}>
@@ -25,10 +67,10 @@ export const Single_follow = ({follow}) => {
             </div>
             {/*TODO button follow, unfollow / podle reality*/}
             <div className={'center-height'}>
-                {follow.followed ? <button className={'follow-button'}>
+                {follow.followed ? <button className={'follow-button'} onClick={unfollowUser}>
                         Unfollow
                     </button> :
-                    <button className={'follow-button'}>
+                    <button className={'follow-button'} onClick={followUser}>
                         Follow
                     </button>
                 }
